5514ed436e5ab7f9eac13a28f3d999e30ea3df95
amboxer21 <amboxer21@gmail.com>
2019-02-11 11:38:42 -0500

    Refactored crontab daemon

--- src/crontab/is_youtube2mp3_running.sh
+++ src/crontab/is_youtube2mp3_running.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if (( `/bin/ps aux | /bin/egrep -i "^root.*[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/youtube2mp3.py" | wc -l` < 1 )); then 
-    logger -i -t youtube2mp3 "Youtube2mp3 is not running, starting youtube2mp3." -f /var/log/youtube2mp3.log
-    /usr/bin/sudo /usr/bin/python /usr/local/bin/youtube2mp3.py &
-elif (( `/bin/ps aux | /bin/egrep -i "^root.*[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/youtube2mp3.py" | wc -l` >= 2 )); then 
-    logger -i -t youtube2mp3 "Killing youtube2mp3." -f /var/log/youtube2mp3.log
-    /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /bin/egrep -i "^root.*[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/youtube2mp3.py" | /usr/bin/awk '{print $2}'`;
-    /usr/bin/sudo /usr/bin/python /usr/local/bin/youtube2mp3.py &
-else
-    logger -i -t youtube2mp3 "Youtube2mp3 is already running." -f /var/log/youtube2mp3.log
+path='/usr/bin'
+y2m='youtube2mp3'
+logfile="/var/log/${y2m}.log"
+youtube2mp3="/usr/local/bin/${y2m}.py"
+
+if (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` < 1 )); then 
+    $path/logger -i -t youtube2mp3 "Youtube2mp3 is not running, starting youtube2mp3." -f $logfile;
+    $path/sudo $path/python $youtube2mp3 &
+    exit;
+elif (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` > 2 )); then 
+    $path/logger -i -t youtube2mp3 "Killing youtube2mp3." -f $logfile;
+    $path/sudo /bin/kill -9 `/bin/ps aux | $path/awk '/[0-9]{1,2}:[0-9]{1,2} .usr.bin.python .usr.local.bin.youtube2mp3/{print $2}'`
+    $path/sudo $path/python $youtube2mp3 &
+    if (( $? != 0 ));  then
+        $path/logger -i -t youtube2mp3 'Failed to kill processes!' -f $logfile;
+    fi
+    exit;
+elif (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` == 1 )); then
+    $path/logger -i -t youtube2mp3 "Youtube2mp3 is already running." -f $logfile;
+    exit;
 fi
