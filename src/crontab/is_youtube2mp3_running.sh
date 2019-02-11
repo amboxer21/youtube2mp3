c3298a0b67632a11010c5f3e97d957da402bd4fa
amboxer21 <amboxer21@gmail.com>
2019-02-11 12:39:09 -0500

    Refactored cron daemon again.

--- src/crontab/is_youtube2mp3_running.sh
+++ src/crontab/is_youtube2mp3_running.sh
@@ -5,19 +5,20 @@ y2m='youtube2mp3'
 logfile="/var/log/${y2m}.log"
 youtube2mp3="/usr/local/bin/${y2m}.py"
 
-if (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` < 1 )); then 
+count=`/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l`;
+
+if [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l` -eq 0 ]]; then 
     $path/logger -i -t youtube2mp3 "Youtube2mp3 is not running, starting youtube2mp3." -f $logfile;
     $path/sudo $path/python $youtube2mp3 &
-    exit;
-elif (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` > 2 )); then 
+elif [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l` -gt 3 ]]; then 
+    /bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}'
     $path/logger -i -t youtube2mp3 "Killing youtube2mp3." -f $logfile;
-    $path/sudo /bin/kill -9 `/bin/ps aux | $path/awk '/[0-9]{1,2}:[0-9]{1,2} .usr.bin.python .usr.local.bin.youtube2mp3/{print $2}'`
+    $path/sudo /bin/kill -9 `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print $2}'`;
     $path/sudo $path/python $youtube2mp3 &
-    if (( $? != 0 ));  then
-        $path/logger -i -t youtube2mp3 'Failed to kill processes!' -f $logfile;
-    fi
-    exit;
-elif (( `/bin/ps aux | $path/awk "/$regex/{print}" | wc -l` == 1 )); then
+elif [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l` -eq 1 ]]; then
+    /bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}'
     $path/logger -i -t youtube2mp3 "Youtube2mp3 is already running." -f $logfile;
-    exit;
+else
+    /bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}'
+    echo -e "$count";
 fi
