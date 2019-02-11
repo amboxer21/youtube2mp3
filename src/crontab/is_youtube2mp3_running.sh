7cb4e2317782feed566079ff42ae779eb3ca1b73
amboxer21 <amboxer21@gmail.com>
2019-02-11 12:51:21 -0500

    Removed unnecessary debug statments.

--- src/crontab/is_youtube2mp3_running.sh
+++ src/crontab/is_youtube2mp3_running.sh
@@ -5,8 +5,6 @@ y2m='youtube2mp3'
 logfile="/var/log/${y2m}.log"
 youtube2mp3="/usr/local/bin/${y2m}.py"
 
-count=`/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l`;
-
 if [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l` -eq 0 ]]; then 
     $path/logger -i -t youtube2mp3 "Youtube2mp3 is not running, starting youtube2mp3." -f $logfile;
     $path/sudo $path/python $youtube2mp3 &
@@ -18,7 +16,4 @@ elif [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~
 elif [[ `/bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}' | wc -l` -eq 1 ]]; then
     /bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}'
     $path/logger -i -t youtube2mp3 "Youtube2mp3 is already running." -f $logfile;
-else
-    /bin/ps aux | $path/awk '/^root.*:[0-9]{1,2}.*youtube2mp3.py/{if($11 !~ /sudo|vim|awk/) print}'
-    echo -e "$count";
 fi
